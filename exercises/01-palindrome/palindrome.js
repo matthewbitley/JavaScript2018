6d269e5e6e7b7c6fce3037f54eefc9dc30756da7
Matthew Bitley <matthew.bitley@gmail.com>
2018-09-10 20:50:34 -0400

    I tried

--- exercises/01-palindrome/palindrome.js
+++ exercises/01-palindrome/palindrome.js
@@ -4,7 +4,25 @@
  * @param  {[string]}  string
  * @return {Boolean}
  */
-function isPalindrome(str) {}
+
+eye = true;
+racecar = true;
+dog = false;
+
+function isPalindrome(str) {
+  
+  str.split('').reverse().join('') === str
+  // Stack Overflow Answer
+  /*var len = str.length;
+    for ( var i = 0; i < Math.floor(len/2); i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;*/
+}
+
+isPalindrome("eye");
 
 module.exports = {
   isPalindrome
