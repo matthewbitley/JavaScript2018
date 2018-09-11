ea4350a75c5b30b1b3450dbb0cf4026d3a406154
Matthew Bitley <matthew.bitley@gmail.com>
2018-09-11 18:46:56 -0400

    Add files via upload
    I tried

---
+++ palindrome.js
@@ -0,0 +1,30 @@
+/**
+ * A palindrom is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+ * Checks to see if a string is a palindrome. You must remove special characters.
+ * @param  {[string]}  string
+ * @return {Boolean}
+ */
+
+eye = true;
+racecar = true;
+dog = false;
+
+function isPalindrome(str) {
+  
+  str.split('').reverse().join('') === str
+  
+  // Stack Overflow Answer
+  /*var len = str.length;
+    for ( var i = 0; i < Math.floor(len/2); i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;*/
+}
+
+isPalindrome("eye");
+
+module.exports = {
+  isPalindrome
+};
