09db8672f1309b5bfe6bd6be8e2a4f2c3f16b5cc
Matthew Bitley <matthew.bitley@gmail.com>
2018-11-07 19:01:48 -0500

    11/07 Commit

--- exercises/18-recursion/recursion.js
+++ exercises/18-recursion/recursion.js
@@ -8,8 +8,9 @@
  */
 
 function fib(n) {
-  if (n <= 1) return 1;
-  return fib(n) = fib(n-1) + fib(n-2);
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fib(n-1) + fib(n-2);
 }
 
 module.exports = {
