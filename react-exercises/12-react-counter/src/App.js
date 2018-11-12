540e8596f1a0c2e97e90f51e4ebec040c78a7013
Matthew Bitley <matthew.bitley@gmail.com>
2018-11-12 18:56:59 -0500

    11/12 Commit

--- react-exercises/12-react-counter/src/App.js
+++ react-exercises/12-react-counter/src/App.js
@@ -17,8 +17,6 @@ class App extends React.Component {
       count: 0
     };
   }
-<<<<<<< HEAD
-
   incClick = () => {
     this.setState({ count: this.state.count + 1 });
     /*if ( this.state.count > 9 ) {
@@ -44,30 +42,8 @@ class App extends React.Component {
         <button onClick={this.decClick} className="dec">Decrement!</button>
         <button onClick={this.resClick} className="reset">Reset</button>
         {this.state.count < 11 ? <h1>Current Count: {this.state.count}</h1> : null }
-=======
-  Increment = () => {
-    let newCount = this.state.count + 1;
-    this.setState({
-      count: newCount
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button className="inc" onClick={this.Increment}>
-          Increment!
-        </button>
-        <button className="dec">Decrement!</button>
-        <button className="reset">Reset</button>
-        <h1>Current Count: {this.state.count}</h1>
->>>>>>> c3fd38596f2bc620fa11025af27d348b46543c6c
       </div>
     );
   }
 }
-
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> c3fd38596f2bc620fa11025af27d348b46543c6c
